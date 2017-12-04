617d5afdc8eaa2efaa9202ebfe9c28f57b53a80e
Yonah Forst <yonaforst@hotmail.com>
2017-12-04 16:23:29 +0100

    added example

---
+++ example/index.js
@@ -0,0 +1,25 @@
+
+const BabelBot = require('../src')
+const config = require('./secrets.example.json')
+
+const bablebot = new BabelBot(config)
+
+module.exports.handler = (event, context, callback) => {
+  const {
+    body,
+    pathParameters,
+  } = event
+
+  const { serviceName } = pathParameters
+  const parsed = JSON.parse(body)
+
+  return bablebot.parse(serviceName, parsed)
+    .then(res => {
+      res.text = "You said: " + res.text
+      return bablebot.send(serviceName, res)
+    })
+    .then(() => callback(null, { statusCode: 200 }))
+    .catch(callback)
+}
+
+
