cae7a1111ac923b75bbb30c1c9f9ea1f9d196d18
Yonah Forst <yonaforst@hotmail.com>
2017-12-06 12:39:11 +0100

    fix line auth

--- src/services/line/makeRequest.js
+++ src/services/line/makeRequest.js
@@ -2,12 +2,11 @@ const fetch = require('../../lib/fetch')
 const baseUrl = 'https://api.line.me'
 
 module.exports = ({ channelAccessToken }) => path => body => {
-	
 	return fetch(baseUrl + path, {
 		body: JSON.stringify(body),
 		method: 'POST',
 	  headers: {
-	    Authorization: `Basic ${channelAccessToken}`,
+	    Authorization: `Bearer ${channelAccessToken}`,
 	    'Content-Type': 'application/json',
 	  },
 	})
