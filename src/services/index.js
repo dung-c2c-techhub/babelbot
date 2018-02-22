ab50e9a6919873e984e6a22a5e22921266a0382e
Yonah Forst <yonaforst@hotmail.com>
2018-02-22 13:11:47 +0100

    added oxygen8

--- src/services/index.js
+++ src/services/index.js
@@ -5,6 +5,7 @@ module.exports = {
   skype: require('./skype'),
   telegram: require('./telegram'),
   twilio: require('./twilio'),
+  oxygen8: require('./oxygen8'),
   loopback: require('./loopback'),
   // wechat: require('./wechat')
 }
