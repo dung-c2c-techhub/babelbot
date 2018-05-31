8f26466760ea9b41bd1f32517e479322acc069a7
Albert Navas <albertnm76@gmail.com>
2018-05-31 09:24:22 +0200

    Fix tabs and remove secret

--- src/services/viber/makeRequest.js
+++ src/services/viber/makeRequest.js
@@ -3,11 +3,11 @@ const baseUrl = 'https://chatapi.viber.com/pa'
 
 module.exports = ({ apiKey }) => path => body => {
 	return fetch(baseUrl + apiKey + path, {
-		body: JSON.stringify(body),
-		method: 'POST',
-	  headers: {
-			'Content-Type': 'application/json',
-			'X-Viber-Auth-Token': apiKey
-	  },
+        body: JSON.stringify(body),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Viber-Auth-Token': apiKey
+        },
 	})
 }
