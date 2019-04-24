51c6b8a7cd5078471a82c7db1d932f3661525f35
joanescalante <joan.escalante@gmail.com>
2019-04-24 15:44:17 +0200

    test mitto4

--- src/services/mitto/makeRequest.js
+++ src/services/mitto/makeRequest.js
@@ -2,13 +2,19 @@ const fetch = require('../../lib/fetch')
 const qs = require('querystring')
 const baseUrl = 'https://api.mitto.ch'
 
-module.exports = (event) => path => body => {
-    console.log('makeRequest', event)
-    var body = qs.stringify({ key: API_key, message: text, from, to })
-    console.log('makeRequest', body)
+module.exports = ({ API_key }) => path => ({ text, from , to }) => {
     
+    let strike = qs.stringify({ 
+        key: API_key, 
+        message: text,
+        from,
+        to
+    })
+
+    console.log('makeRequest', strike)
+
     return fetch(baseUrl + path, {
-        body,
+        body: strike,
         method: 'POST',
         headers: {
             'Content-Type': 'multipart/form-data',
