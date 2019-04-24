188485d211912c017a1c1bb8dbd9c01f31d6d217
joanescalante <joan.escalante@gmail.com>
2019-04-24 15:54:52 +0200

    test mitto5

--- src/services/mitto/index.js
+++ src/services/mitto/index.js
@@ -6,7 +6,7 @@ const flow = require('lodash.flow')
 
 module.exports = config => {
     console.log('index - config', config)
-    const sendFunc = flow(format(config), makeRequest(config)('/sms/'))
+    const sendFunc = flow(format(config), makeRequest(config)('/sms'))
     console.log('sendFunc', sendFunc)
     return {
         parse,
--- src/services/mitto/makeRequest.js
+++ src/services/mitto/makeRequest.js
@@ -4,15 +4,15 @@ const baseUrl = 'https://api.mitto.ch'
 
 module.exports = ({ API_key }) => path => ({ text, from , to }) => {
     
-    let strike = qs.stringify({ 
+    let strike = { 
         key: API_key, 
         message: text,
         from,
         to
-    })
-
+    }
     console.log('makeRequest', strike)
-
+    console.log('path',path)
+    console.log('urk', baseUrl + path)
     return fetch(baseUrl + path, {
         body: strike,
         method: 'POST',
