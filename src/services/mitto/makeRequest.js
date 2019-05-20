40faf3e1336316e76ce049d2b76d1ece732bb150
joanescalante <joan.escalante@gmail.com>
2019-05-20 15:40:28 +0200

    mitto

--- src/services/mitto/makeRequest.js
+++ src/services/mitto/makeRequest.js
@@ -2,8 +2,11 @@ const fetch = require('../../lib/fetch')
 const qs = require('querystring')
 const baseUrl = 'https://api.mitto.ch'
 
-module.exports = ({ API_key }) => path => (event) => {
+module.exports = (config) => path => (event) => {
+
     console.log('event ---------',event )
+    console.log('config ---------',config )
+    const { API_key } = config
     const { text, from , to } = event
     let strike = { 
         "key": API_key, 
