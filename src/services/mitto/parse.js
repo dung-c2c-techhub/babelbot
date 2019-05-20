2077605849685bd814c936c4322f56c0da62c8bc
joanescalante <joan.escalante@gmail.com>
2019-05-20 16:54:32 +0200

    mitto

--- src/services/mitto/parse.js
+++ src/services/mitto/parse.js
@@ -1,15 +1,14 @@
 const SERVICE_NAME = 'mitto'
 
 module.exports = (event) => {
-    const { receiver, content } = event
-    console.log('receiver parse------------>', event)
-    var msg = {
+    const { sender, receiver, content } = event
+    
+    const msg = {
         service_name: SERVICE_NAME,
-        service_user_id: receiver,
+        service_user_id: `${sender}:${receiver}`,
         text: content,
         timestamp: Date.now(),
     }
-    console.log('receiver parse------------>', msg)
 
     return Promise.resolve([msg])
 }
