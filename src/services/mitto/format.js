76fb61ece820df8402ba1109b2c119f2a37d9eb6
joanescalante <joan.escalante@gmail.com>
2019-05-20 15:08:26 +0200

    mitto

--- src/services/mitto/format.js
+++ src/services/mitto/format.js
@@ -1,7 +1,7 @@
 module.exports = ( event ) => ( msg ) => {
     const { service_user_id, text } = msg
     var [phone, mask] = service_user_id.split(':')
-
+    console.log('service_user_id--------', service_user_id)
     return {
         text,
         to: phone,
