7a7d0f31ef88343f2b82a289c5113277d01af4f2
joanescalante <joan.escalante@gmail.com>
2019-05-21 08:41:47 +0200

    unicode

--- src/services/mitto/makeRequest.js
+++ src/services/mitto/makeRequest.js
@@ -2,19 +2,15 @@ const fetch = require('../../lib/fetch')
 const qs = require('querystring')
 const baseUrl = 'https://api.mitto.ch'
 
-module.exports = (config) => path => (event) => {
-
-    console.log('event ---------',event )
-    console.log('config ---------',config )
-    const { API_key } = config
-    const { text, from , to } = event
+module.exports = ({ API_key }) => path => ({ text, from , to }) => {    
     let strike = { 
         "key": API_key, 
         "message": text,
         "from": from,
-        "to": to
+        "to": to,
+        "type":'unicode'
     }
-console.log('-------strike---------',strike)
+
     return fetch(baseUrl + path, {
         method: 'POST',
         body: JSON.stringify(strike),
