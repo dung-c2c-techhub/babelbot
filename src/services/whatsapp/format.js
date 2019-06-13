3148f6fc6698c2d042ca297712a4c0cc11f0fe8a
joanescalante <joan.escalante@gmail.com>
2019-06-13 18:59:37 +0200

    changes

--- src/services/whatsapp/format.js
+++ src/services/whatsapp/format.js
@@ -1,7 +1,11 @@
-module.exports = ({ messagingServiceSid }) => ({ service_user_id, text }) => {
+module.exports = (event) => (msg) => {
+    console.log('format event---- >', event)
+    console.log('format msg ---- >', msg)
+    const { service_user_id, text } = msg
     return {
         To: service_user_id,
         MessagingServiceSid: messagingServiceSid,
-        Body: text
+        Body: text,
+
     }
 }
