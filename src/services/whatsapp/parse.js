07043892dbd1fd0c197c08fe957797d084f2203f
joanescalante <joan.escalante@gmail.com>
2019-06-14 09:29:37 +0200

    contentTypeXml

--- src/services/whatsapp/parse.js
+++ src/services/whatsapp/parse.js
@@ -1,9 +1,8 @@
 const SERVICE_NAME = 'whatsapp'
 
 module.exports = (parseEvent) => {
-    console.log('parseEvent---->',parseEvent)
     const { From, Body } = parseEvent
-    var msg = {
+    const msg = {
         service_name: SERVICE_NAME,
         service_user_id: From,
         text: Body,
