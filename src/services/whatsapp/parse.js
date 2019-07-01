94e23eb4ea3fd54dd1b44c14de174cda1cff5c1d
Dani <dtelleslopez@gmail.com>
2019-07-01 16:36:15 +0200

    Allow whatsapp service to send images

--- src/services/whatsapp/parse.js
+++ src/services/whatsapp/parse.js
@@ -1,13 +1,17 @@
 const SERVICE_NAME = 'whatsapp'
 
-module.exports = (parseEvent) => {
-    const { From, Body } = parseEvent
+module.exports = ({ From, Body, MediaUrl0, MediaContentType0 }) => {
     const msg = {
         service_name: SERVICE_NAME,
         service_user_id: From,
-        text: Body,
         timestamp: Date.now(),
+    };
+
+    if (MediaContentType0 && MediaContentType0.includes('image') && MediaUrl0) {
+        msg.attachments = [{ url: MediaUrl0 }];
+    } else {
+        msg.text = Body;
     }
 
-    return Promise.resolve([msg])
+    return Promise.resolve([msg]);
 }
